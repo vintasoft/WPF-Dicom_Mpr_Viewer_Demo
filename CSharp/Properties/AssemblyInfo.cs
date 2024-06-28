99b45dee3c7905aaec6fa68c58016bdc8b29c56a
vintasoft <alex@vintasoft.com>
2024-06-28 10:11:20 +0300

    Source codes update

--- CSharp/Properties/AssemblyInfo.cs
+++ CSharp/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Windows;
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
 )]
 
-[assembly: AssemblyVersion("12.4.6.1")]
-[assembly: AssemblyFileVersion("12.4.6.1")]
+[assembly: AssemblyVersion("12.4.7.1")]
+[assembly: AssemblyFileVersion("12.4.7.1")]
